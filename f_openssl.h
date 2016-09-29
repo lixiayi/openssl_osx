49c56e552ad6745caf6b35dab19411f043e12fcd
lixiaoyi <lixiaoyi!9830715@163.com>
2016-09-29 15:42:38 +0800

    通过openssl的函数生成car
    通过X509_REQ等函数生成csr,通过c语言实现。

---
+++ f_openssl.h
@@ -0,0 +1,14 @@
+//
+//  f_openssl.h
+//  openssl_text
+//
+//  Created by xiaoyi li on 16/9/29.
+//  Copyright © 2016年 xiaoyi li. All rights reserved.
+//
+
+#ifndef f_openssl_h
+#define f_openssl_h
+
+#include <stdio.h>
+
+#endif /* f_openssl_h */
